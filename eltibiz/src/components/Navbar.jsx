00dcaa107f6d3d70c3a3f104cec4fa9efc6e5eb4
PradiptaBagas <98763194+PradiptaBagas@users.noreply.github.com>
2024-05-29 14:02:33 +0700

    modif element nav

--- eltibiz/src/components/Navbar.jsx
+++ eltibiz/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
                     <li><a href="#home">Beranda</a></li>
                     <li><a href="#profil">Profil</a></li>
                     <li><a href="#program">Program</a></li>
-                    <li><a href="#informasi">Informasi</a></li>
+                    <li><a href="#informasi">Informasi Berita</a></li>
                     <li><a href="#artikel">Artikel</a></li>
                     <li><a href="#kontak">Kontak</a></li>
                     <li><a href="" className="tbl-biru">Register</a></li>
